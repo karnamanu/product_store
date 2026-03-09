c9f040b498b16f9f5a710d26f31023674f12d48c
karnamanu <manojbm497@gmai.com>
2026-03-10 03:20:20 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,22 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 dotenv.config();
 
-app.get("/", (req, res) => {
-	res.send("server is ready");
+app.get("/", async (req, res) => {
+	const product = req.body;
+	if (!product.name || !product.price || !product.image) {
+		return res
+			.status(400)
+			.json({ success: false, message: "Please provide all fields" });
+	}
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({ success: true, data: newProduct });
+	} catch (error) {
+		console.error("Error in Create Product:", error.message);
+		res.status(500).json({ success: false, message: "Server Error" });
+	}
 });
 
 app.listen(5000, () => {
